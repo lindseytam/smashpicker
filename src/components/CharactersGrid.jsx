cae922c000de25fc0ce59291b77320f3c3b194d8
Lindsey <lindseytam@gmail.com>
2021-09-26 18:25:31 -0700

    fornat char grid

--- src/components/CharactersGrid.jsx
+++ src/components/CharactersGrid.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './../styles/CharactersGridStyle.css'
 
 function CharactersGrid (props) {
-  const { charData, tagData } = props
+  const { charData } = props
   const data = (charData.length === 0) ? [] : Object.keys(charData[0])
 
   const renderImg = (item) => {
     const imgPath = charData[0][item].Img
     return (
-        <img src={imgPath}/>
+      <div className="column">
+        <div className="card">
+          <img className="Char-Img" src={imgPath}/>
+        </div>
+      </div>
+
     )
   }
   return (
-
-    <div>
-       {data.map((item) => renderImg(item))}
+    <div className="Char-Grid">
+       <div className="row">
+         {data.map((item) => renderImg(item))}
+       </div>
     </div>
   )
 }
