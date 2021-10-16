6068f525c6badc5506ee52f91f09855a6151bf1a
corey hu <corey.huym@gmail.com>
2021-10-15 17:30:17 -0700

    remove duplicate import

--- src/pages/ResultScreen.jsx
+++ src/pages/ResultScreen.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types'
 import { Button } from '../components/Button'
 import Generate from '../components/Generate'
 import SelectedCharacter from '../components/SelectedCharacter'
-
-import SelectedCharacter from '../components/SelectedCharacter'
 function ResultScreen (props) {
   const { numPlayers, theme, chosenChars, setChosenChars, omitChars, tagData, unique, charData } = props
   const [shuffle, setShuffle] = useState(false)
