c4c2115a0f18cce012a94d927869d22adc9f93d1
Lindsey <lindseytam@gmail.com>
2021-10-27 20:54:21 -0700

    start at the top

--- src/pages/ResultScreen.jsx
+++ src/pages/ResultScreen.jsx
@@ -15,6 +15,9 @@ function ResultScreen (props) {
     setImgsToLoad(chosenChars.length - 1)
   }, [chosenChars])
 
+  // make window start at the top
+  document.querySelector('body').scrollTo(0, 0)
+
   // helper function to wait X seconds
   const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
